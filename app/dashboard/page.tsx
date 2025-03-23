1daaa7c61acd04452e01f7fe9f01457f98d98593
Joe B <joeybtelevision@gmail.com>
2025-03-23 19:37:56 -0400

    Test commit with new git config

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
  
 export default async function Page() {
+  {//
+  }
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
