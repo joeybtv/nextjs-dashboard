f0b1eae0053071bdca0a102761b39a2142037159
Joe B <joeybtelevision@gmail.com>
2025-03-29 12:42:42 -0400

    Linting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -76,7 +76,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     // Validate form using Zod
-    const validatedFields = CreateInvoice.safeParse({
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
